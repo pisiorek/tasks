03c273165db8214739b073da15b2bfc84ce2d53b
Dariusz Harańczyk <darek.haranczyk@gmail.com>
2019-04-20 13:39:39 +0200

    19.1 Zmiany w plikach konfiguracyjnych dla potrzeb modułu 19. Połączenie Trello z Heroku.

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -2,6 +2,8 @@ package com.crud.tasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 //public class TasksApplication extends SpringBootServletInitializer {  //przy tomcacie odkomentować i zakomentować ponizsza linijke
@@ -11,11 +13,12 @@ public class TasksApplication {
 		SpringApplication.run(TasksApplication.class, args);
 	}
 
-	/*
+/*
 	@Override
 	protected SpringApplicationBuilder configure (SpringApplicationBuilder application){
 		return application.sources(TasksApplication.class);
-	}*/
+	}
+*/
 
 
 }
