f66a46b484420608a9ae5c7ba79af4f425b6ed13
Dariusz Harańczyk <darek.haranczyk@gmail.com>
2019-05-04 19:23:25 +0200

    21.1_TestyFasady_mockowanie-korekta testów i uzupełnienie

--- src/test/java/com/crud/tasks/trello/facade/FacadeTrelloTest.java
+++ src/test/java/com/crud/tasks/trello/facade/FacadeTrelloTest.java
@@ -8,6 +8,7 @@ import com.crud.tasks.mapper.TrelloMapper;
 import com.crud.tasks.service.TrelloService;
 import com.crud.tasks.trello.client.TrelloClient;
 import com.crud.tasks.trello.config.TrelloConfig;
+import com.crud.tasks.trello.validator.TrelloValidator;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -28,7 +29,7 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class FacadeTrelloTest {
     @InjectMocks
-    private TrelloClient trelloClient;
+    private TrelloFacade trelloFacade;      // @InjectMocks podpinamy do klasy którą testujemy
     @Mock
     private RestTemplate restTemplate;
     @Mock
@@ -37,6 +38,10 @@ public class FacadeTrelloTest {
     private TrelloService trelloService;
     @Mock
     private TrelloMapper trelloMapper;
+    @Mock
+    private TrelloClient trelloClient;
+    @Mock
+    private TrelloValidator trelloValidator;
 
     @Before
     public void init(){
@@ -45,73 +50,8 @@ public class FacadeTrelloTest {
         when(trelloConfig.getTrelloToken()).thenReturn("test");
     }
 
-    @Test
-    public  void shouldFetchTrelloBoards() throws URISyntaxException {
-        //Given
-        TrelloBoardDto[] trelloBoards = new TrelloBoardDto[1];
-        trelloBoards[0] = new TrelloBoardDto("test_id", "test_board", new ArrayList<>());
-
-        URI uri = new URI("http://test.com/members/dariuszharanczyk/boards?key=test&token=test&fields=name,id&lists=all");
-        //System.out.println(uri);
-        when(restTemplate.getForObject(uri, TrelloBoardDto[].class)).thenReturn(trelloBoards);
-
-        //When
-        List<TrelloBoardDto> fetchedTrelloBoards = trelloClient.getTrelloBoards();
-        //List<TrelloBoardDto> fetchedTrelloBoards = trelloService.fetchTrelloBoards();
-
-        //Then
-        assertEquals(1, fetchedTrelloBoards.size());
-        assertEquals("test_id", fetchedTrelloBoards.get(0).getId());
-        assertEquals("test_board", fetchedTrelloBoards.get(0).getName());
-        assertEquals(new ArrayList<>(), fetchedTrelloBoards.get(0).getLists());
-
-    }
-    @Test
-    public void shouldCreateCard() throws URISyntaxException{
-        //Given
-        TrelloCardDto trelloCardDto = new TrelloCardDto(
-                "Test task",
-                "Test Description",
-                "top",
-                "test_id"
-        );
-        URI uri = new URI("http://test.com/cards?key=test&token=test&name=Test%20task&desc=Test%20Description&pos=top&idList=test_id");
-
-        CreatedTrelloCardDto createdTrelloCardDto = new CreatedTrelloCardDto(
-                "1",
-                "Test task",
-                "http://test.com"
-        );
-
-        when(restTemplate.postForObject(uri, null, CreatedTrelloCardDto.class)).thenReturn(createdTrelloCardDto);
-
-        //When
-        CreatedTrelloCardDto newCard = trelloClient.createNewCard(trelloCardDto);
-
-        //Then
-        assertEquals("1", newCard.getId());
-        assertEquals("Test task", newCard.getName());
-        assertEquals("http://test.com", newCard.getShortUrl());
-
-    }
 
     @Test
-    public void  shouldReturnEmptyList()throws URISyntaxException{
-        //Given
-        TrelloBoardDto[] trelloBoards = null;
-
-        URI uri = new URI("http://test.com/members/dariuszharanczyk/boards?key=test&token=test&fields=name,id&lists=all");
-
-        when(restTemplate.getForObject(uri, TrelloBoardDto[].class)).thenReturn(trelloBoards);
-
-        //When
-        List<TrelloBoardDto> fetchedTrelloBoards = trelloClient.getTrelloBoards();
-
-        //Then
-        assertEquals(0, fetchedTrelloBoards.size());
-    }
-
-/*    @Test
     public void  shouldReturnEmptyListWithFacade()throws URISyntaxException{
         //Given
         TrelloBoardDto[] trelloBoards = null;
@@ -125,7 +65,7 @@ public class FacadeTrelloTest {
 
         //Then
         assertEquals(0, fetchedTrelloBoards.size());
-    }*/
+    }
 
     @Test
     public  void shouldFetchTrelloBoardsWithFacade() throws URISyntaxException {
@@ -136,13 +76,14 @@ public class FacadeTrelloTest {
         TrelloBoard[] trelloB = new TrelloBoard[1];
         trelloB[0] = new TrelloBoard("test_id", "test_board", new ArrayList<>());
 
-        TrelloFacade trelloFacade= new TrelloFacade();
-
         URI uri = new URI("http://test.com/members/dariuszharanczyk/boards?key=test&token=test&fields=name,id&lists=all");
 
-       // when(restTemplate.getForObject(uri, TrelloBoardDto[].class)).thenReturn(trelloBoards);
-        when(trelloService.fetchTrelloBoards()).thenReturn(Arrays.asList(trelloBoards));
+        /***
+         * Zawsze mockujemy wszystkie metody klas, które są wykorzystywane przez testowaną klasę
+         */
         when(trelloMapper.mapToBoards(trelloService.fetchTrelloBoards())).thenReturn(Arrays.asList(trelloB));
+        when(trelloValidator.validateTrelloBoards(Arrays.asList(trelloB))).thenReturn(Arrays.asList(trelloB));
+        when(trelloMapper.mapToBoardsDto(Arrays.asList(trelloB))).thenReturn(Arrays.asList(trelloBoards));
 
         //When
         List<TrelloBoardDto> fetchedTrelloBoards = trelloFacade.fetchTrelloBoards();
