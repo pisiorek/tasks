f5baa38a276b71ca07630eb22617d7949a9f7c08
Dariusz Harańczyk <darek.haranczyk@gmail.com>
2019-05-27 09:25:25 +0200

    24.1_Thymeleaf_przywrócony SpringBootInitializer w TaskApplication

--- src/main/java/com/crud/tasks/controller/StaticWebPageController.java
+++ src/main/java/com/crud/tasks/controller/StaticWebPageController.java
@@ -3,11 +3,14 @@ package com.crud.tasks.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
+
 @Controller
 public class StaticWebPageController {
 
     @RequestMapping("/")
     public String index(){
+
         return "index";
     }
 }
