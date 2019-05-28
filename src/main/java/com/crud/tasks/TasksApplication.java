90cad7066b5a8070a27dce45e04ad9584d1c6f60
Dariusz Harańczyk <darek.haranczyk@gmail.com>
2019-05-28 20:39:04 +0200

    24.2 Implementacja metody createMimeMessage tworzącej nowy template wiadomosci email

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -6,19 +6,17 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-//public class TasksApplication extends SpringBootServletInitializer {  //przy tomcacie odkomentować i zakomentować ponizsza linijke
-public class TasksApplication {		//przy heroku odkomentować i zakomentować powyższe
+public class TasksApplication extends SpringBootServletInitializer {  //przy tomcacie odkomentować i zakomentować ponizsza linijke
+//public class TasksApplication {		//przy heroku odkomentować i zakomentować powyższe
 
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
 	}
 
-/*
 	@Override
 	protected SpringApplicationBuilder configure (SpringApplicationBuilder application){
 		return application.sources(TasksApplication.class);
 	}
-*/
 
 
 }
--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,19 +17,46 @@ public class SimpleEmailService {
     private static final Logger LOGGER = LoggerFactory.getLogger(SimpleMailMessage.class);
 
     @Autowired
+    private MailCreatorService mailCreatorService;
+
+    @Autowired
     private JavaMailSender javaMailSender;
+
+    /****
+        ponizej - obie metody wprowadzone na etapie modułu 24.2
+     */
+    private MimeMessagePreparator createMimeMessage(final Mail mail){
+        return mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(mail.getMailTo());
+            messageHelper.setSubject(mail.getSubject());
+            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
+        };
+
+    }
 
     public void send(final Mail mail){
         LOGGER.info("Starting email preparation...");
         try {
-            javaMailSender.send(createMailMessage(mail));
+            javaMailSender.send(createMimeMessage(mail));
             LOGGER.info("Email has been sent");
         }catch(MailException e){
             LOGGER.error("Failed to process email sending: ", e.getMessage(), e );
         }
     }
 
-    private SimpleMailMessage createMailMessage(final Mail mail){
+/*    private SimpleMailMessage createMailMessage(final Mail mail){
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()));
+        return mailMessage;
+    }*/
+
+
+
+//poniżej pozostałość po wcześniejszych modułach
+/*    private SimpleMailMessage createMailMessage(final Mail mail){
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
@@ -36,5 +65,5 @@ public class SimpleEmailService {
             mailMessage.setCc(mail.getToCc());
         }
         return mailMessage;
-    }
+    }*/
 }
